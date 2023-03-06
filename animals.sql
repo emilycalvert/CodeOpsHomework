e6d38a39fcd8ec52badd456e2c4d4f87db210c4f
Emily Calvert <120228818+emilycalvert@users.noreply.github.com>
2023-03-06 09:15:00 -0800

    animals upload
    first time playing with databases and SQL

---
+++ animals.sql
@@ -0,0 +1,9 @@
+penny	peCOPY 8
+	39	f
+larry	lion	43	20	t
+lacey	lion	22.4	502	f
+lucy	lion	201	32	t
+molly	meerkat	43	21	f
+carl	meerkat	32	34.8	f
+carl	chinchilla	32.21	43.56	t
+emily	human	333	4.21	f
